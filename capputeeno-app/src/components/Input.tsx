590058e0510fe2946c67d9f7c75348af94119e3b
BerettaSM <ramonsaviato@hotmail.com>
2024-02-03 14:09:43 -0300

    refactor: change input to accept refs

--- capputeeno-app/src/components/Input.tsx
+++ capputeeno-app/src/components/Input.tsx
@@ -1,15 +1,19 @@
 'use client';
 
-import styled from "styled-components";
+import React from 'react';
+import { ComponentPropsWithRef } from 'react';
+import styled from 'styled-components';
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+interface InputProps extends ComponentPropsWithRef<'input'> {}
 
-export function Input(props: InputProps) {
-    return <Wrapper {...props} />;
-}
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+    function Input(props, ref) {
+        return <Wrapper {...props} ref={ref} />;
+    }
+);
 
 const Wrapper = styled.input`
-    background-color: var(--bg-secondary);
+    background-color: inherit;
     border: none;
     border-radius: var(--border-radius);
     color: var(--text-dark-2);
