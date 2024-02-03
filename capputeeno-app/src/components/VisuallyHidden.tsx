580329085056ffd9bf9a2ff5c1f2987b82cfc9a9
BerettaSM <ramonsaviato@hotmail.com>
2024-02-03 14:03:47 -0300

    rearrange: add visually hidden component

---
+++ capputeeno-app/src/components/VisuallyHidden.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import styled from 'styled-components';
+
+interface VisuallyHidddenProps {
+    children: React.ReactNode;
+}
+
+export function VisuallyHidden({ children }: VisuallyHidddenProps) {
+    return <Wrapper>{children}</Wrapper>;
+}
+
+const Wrapper = styled.div`
+    position: absolute;
+    overflow: hidden;
+    clip: rect(0 0 0 0);
+    height: 1px;
+    width: 1px;
+    margin: -1px;
+    padding: 0;
+    border: 0;
+`;
