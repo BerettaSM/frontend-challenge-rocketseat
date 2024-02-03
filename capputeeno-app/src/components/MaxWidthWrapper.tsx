c3f0d6bae1465924e81e365590802eca563ce202
BerettaSM <ramonsaviato@hotmail.com>
2024-02-03 12:35:00 -0300

    rearrange: add max width wrapper component

---
+++ capputeeno-app/src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import styled from 'styled-components';
+
+interface MaxWidthWrapperProps {
+    children: React.ReactNode;
+    maxWidth?: number;
+    padding?: number;
+}
+
+export function MaxWidthWrapper({
+    maxWidth = 1120,
+    padding = 16,
+    children,
+}: MaxWidthWrapperProps) {
+    return (
+        <Wrapper $maxwidth={maxWidth} $padding={padding}>
+            {children}
+        </Wrapper>
+    );
+}
+
+const Wrapper = styled.div<{ $maxwidth: number; $padding: number }>`
+    margin-inline: auto;
+    width: 100%;
+    max-width: ${(p) => `${(p.$maxwidth - p.$padding * 2) / 16}rem`};
+    padding-inline: ${(p) => p.$padding}px;
+`;
