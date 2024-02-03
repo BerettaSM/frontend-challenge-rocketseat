88fb083d1dcef5192b2888a11083e6bc17aceb46
BerettaSM <ramonsaviato@hotmail.com>
2024-02-03 15:32:43 -0300

    rearrange: add cart button

---
+++ capputeeno-app/src/components/CartButton.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+import { CartIcon } from './icons';
+import { VisuallyHidden } from '.';
+
+export function CartButton() {
+    return (
+        <Wrapper>
+            <CartIcon />
+            <VisuallyHidden>Your Cart</VisuallyHidden>
+            <ItemCounter>2</ItemCounter>
+        </Wrapper>
+    );
+}
+
+const Wrapper = styled.button`
+    align-items: center;
+    background-color: inherit;
+    border: none;
+    border-radius: var(--border-radius);
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    position: relative;
+    height: 42px;
+    width: 42px;
+`;
+
+const ItemCounter = styled.span`
+    background-color: var(--delete-color);
+    bottom: 0;
+    border-radius: 50%;
+    color: #fff;
+    font-size: 10px;
+    height: 18px;
+    line-height: 18px;
+    position: absolute;
+    right: 0;
+    width: 18px;
+`;
