9c3397ba8a5aa003bb789ce09496d9d496792d67
BerettaSM <ramonsaviato@hotmail.com>
2024-02-04 11:32:56 -0300

    refactor: flesh out more components from product item

--- capputeeno-app/src/components/ProductItem.tsx
+++ capputeeno-app/src/components/ProductItem.tsx
@@ -11,61 +11,59 @@ interface ProductItemProps {
 }
 
 export function ProductItem({ id, title, imageUrl, price }: ProductItemProps) {
-
-    const formattedPrice = 'R$ 43,00'
+    const formattedPrice = 'R$ 43,00';
 
     return (
         <Wrapper>
-            <div>
+            <ImageContainer>
                 <Image
                     src={imageUrl}
                     fill={true}
                     quality={100}
-                    alt='some img'
+                    alt="some img"
                 />
-            </div>
-            <div>
-                <h2>{title}</h2>
-                <hr />
-                <p>{formattedPrice}</p>
-            </div>
+            </ImageContainer>
+            <ProductDetails>
+                <ProductTitle>{title}</ProductTitle>
+                <Separator />
+                <ProductPrice>{formattedPrice}</ProductPrice>
+            </ProductDetails>
         </Wrapper>
-    )
+    );
 }
 
 const Wrapper = styled.article`
     background-color: #fff;
     border-radius: var(--border-radius) var(--border-radius) 0 0;
     overflow: hidden;
-
     height: 378px;
+`;
 
-    & div:first-of-type {
-        position: relative;
-        height: 300px;
-    }
-
-    & div:last-of-type {
-        padding: 8px 12px;
-    }
-
-    & h2 {
-        font-size: 1rem;
-        font-weight: 300;
-    }
-
-    & hr {
-        border: none;
-        border-block-start: 1px solid var(--shapes);
-        margin-block: 8px;
-    }
-
-    & p {
-        font-size: ${14 / 16}rem;
-        font-weight: bold;
-    }
+const ImageContainer = styled.div`
+    position: relative;
+    height: 300px;
 
     & img {
         object-fit: cover;
     }
+`;
+
+const ProductDetails = styled.div`
+    padding: 8px 12px;
+`;
+
+const ProductTitle = styled.h2`
+    font-size: 1rem;
+    font-weight: 300;
+`;
+
+const ProductPrice = styled.p`
+    font-size: ${14 / 16}rem;
+    font-weight: bold;
+`;
+
+const Separator = styled.hr`
+    border: none;
+    border-block-start: 1px solid var(--shapes);
+    margin-block: 8px;
 `;
