f533c73d49f15cbb52a7c33a89c9b9fa09fbc79f
BerettaSM <ramonsaviato@hotmail.com>
2024-02-04 13:35:23 -0300

    rearrange: add price format function

--- capputeeno-app/src/utils/helpers.ts
+++ capputeeno-app/src/utils/helpers.ts
@@ -30,3 +30,13 @@ export function range(
 
     return result;
 }
+
+
+const currencyFormatter = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+})
+
+export function formatCurrency(value: number) {
+    return currencyFormatter.format(value);
+}
